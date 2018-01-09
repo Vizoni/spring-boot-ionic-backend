54aaab78b17c8ab9d9548f13dff69f7c8bb1f630
Raphael Vizoni <ravizoni@gmail.com>
2018-01-09 18:41:07 -0200

    Banco de dados H2 (para teste) e criacao automatica da base de dados

--- src/main/java/com/vizoni/cursomc/domain/Categoria.java
+++ src/main/java/com/vizoni/cursomc/domain/Categoria.java
@@ -2,9 +2,17 @@ package com.vizoni.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L; // numero de versão padrão
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
