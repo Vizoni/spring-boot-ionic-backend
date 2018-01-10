d53e2e2d9095dae071b6facbb1c0902e4de5a294
Raphael Vizoni <ravizoni@gmail.com>
2018-01-09 22:27:29 -0200

    Criando operacao de instanciacao

--- src/main/java/com/vizoni/cursomc/CursomcApplication.java
+++ src/main/java/com/vizoni/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.vizoni.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.vizoni.cursomc.domain.Categoria;
+import com.vizoni.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	/* Este método vem do CommandLineRunner que permite já executar um código quando iniciar,
+	 * está sendo usado para popular as categorias do banco (para que não comece vazio)*/
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		/* cria uma array para não ter q dar um SAVE de cada vez em cada categoria */
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
