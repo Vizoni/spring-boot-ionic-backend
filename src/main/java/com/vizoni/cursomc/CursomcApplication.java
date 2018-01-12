7c3781b22ec20ec521533a9c6b2a7b154ba91ad0
Raphael Vizoni <ravizoni@gmail.com>
2018-01-12 09:38:08 -0200

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/vizoni/cursomc/CursomcApplication.java
+++ src/main/java/com/vizoni/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.vizoni.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.vizoni.cursomc.domain.Cidade;
 import com.vizoni.cursomc.domain.Cliente;
 import com.vizoni.cursomc.domain.Endereco;
 import com.vizoni.cursomc.domain.Estado;
+import com.vizoni.cursomc.domain.Pagamento;
+import com.vizoni.cursomc.domain.PagamentoComBoleto;
+import com.vizoni.cursomc.domain.PagamentoComCartao;
+import com.vizoni.cursomc.domain.Pedido;
 import com.vizoni.cursomc.domain.Produto;
+import com.vizoni.cursomc.domain.enums.EstadoPagamento;
 import com.vizoni.cursomc.domain.enums.TipoCliente;
 import com.vizoni.cursomc.repositories.CategoriaRepository;
 import com.vizoni.cursomc.repositories.CidadeRepository;
 import com.vizoni.cursomc.repositories.ClienteRepository;
 import com.vizoni.cursomc.repositories.EnderecoRepository;
 import com.vizoni.cursomc.repositories.EstadoRepository;
+import com.vizoni.cursomc.repositories.PagamentoRepository;
+import com.vizoni.cursomc.repositories.PedidoRepository;
 import com.vizoni.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -96,5 +110,24 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1,e2));
+		
+		// =============================================================================
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm"); // mascara de formatação pra instanciar uma data
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("12/08/2016 08:55"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);		
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 15:44"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1,ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1,pagto2));
+		
 	}
 }
