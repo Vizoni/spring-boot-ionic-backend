977f560522fb5f45c3081b0cb09df2eff57b4ef7
Raphael Vizoni <ravizoni@gmail.com>
2018-01-15 15:58:35 -0200

    Inserindo uma nova Categoria com POST

--- src/main/java/com/vizoni/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vizoni/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.vizoni.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vizoni.cursomc.domain.Categoria;
 import com.vizoni.cursomc.services.CategoriaService;
@@ -26,10 +29,20 @@ public class CategoriaResource {
 		a função retorna um ResponseEntity pq ele já é próprio pra requisições HTTP (traz o código, status etc)
 		o ? quer dizer q pode retornar qlqer entity */
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 		/* Retorna a resposta (obj) dentro do BODY do HTTP (se estiver ok) */
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		/* O fromCurrentRequest pega o endpoint (nesse caso /categorias)
+		 * dps concatena com o ID recebido do objeto através do buildAndExpand e converte ele para URI */
+		return ResponseEntity.created(uri).build();
+		/* retorna o created baseado na URI montada*/
 	}
 	
 }
--- src/main/java/com/vizoni/cursomc/services/CategoriaService.java
+++ src/main/java/com/vizoni/cursomc/services/CategoriaService.java
@@ -13,13 +13,20 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar (Integer id) {
+	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id +
 											  ", Tipo: " + Categoria.class.getName());
 		}
 		return obj;
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		/* seta o Id como nulo para garantir que é um objeto novo
+		 * se o ID viesse preenchido, ele ATUALIZARIA com o método save, ao invés de CRIAR um novo*/
+		return repo.save(obj);
 	}
 	
 }
