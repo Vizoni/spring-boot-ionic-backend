eb5014b326d001d42f9ac56d21797b19711f6623
Raphael Vizoni <ravizoni@gmail.com>
2018-01-17 15:20:20 -0200

    Validacao customizada: email não repetido na inserção de Cliente

--- src/main/java/com/vizoni/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/vizoni/cursomc/repositories/ClienteRepository.java
@@ -2,9 +2,14 @@ package com.vizoni.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.vizoni.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	// não conta como transação do banco, deixando mais rápido (?)
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/vizoni/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/vizoni/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vizoni.cursomc.domain.Cliente;
 import com.vizoni.cursomc.domain.enums.TipoCliente;
 import com.vizoni.cursomc.dto.ClienteNewDTO;
+import com.vizoni.cursomc.repositories.ClienteRepository;
 import com.vizoni.cursomc.resources.exceptions.FieldMessage;
 import com.vizoni.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +33,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			// se tiver achado um cliente pelo email, lança exceçao de email duplicado
+			list.add(new FieldMessage("email", "Email já cadastrado!"));
 		}
 		
 		for (FieldMessage e : list) {
