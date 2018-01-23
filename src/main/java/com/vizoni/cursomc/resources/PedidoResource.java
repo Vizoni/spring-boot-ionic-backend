4694ddaf4428ee91f83a6586c73d9072551d1b36
Raphael Vizoni <ravizoni@gmail.com>
2018-01-23 15:57:25 -0200

    Inserindo Pedido

--- src/main/java/com/vizoni/cursomc/resources/PedidoResource.java
+++ src/main/java/com/vizoni/cursomc/resources/PedidoResource.java
@@ -1,14 +1,17 @@
 package com.vizoni.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vizoni.cursomc.domain.Pedido;
 import com.vizoni.cursomc.services.PedidoService;
@@ -26,5 +29,18 @@ public class PedidoResource {
 		return ResponseEntity.ok().body(obj);
 		/* Retorna a resposta (obj) dentro do BODY do HTTP (se estiver ok) */
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		/* O fromCurrentRequest pega o endpoint (nesse caso /categorias)
+		 * dps concatena com o ID recebido do objeto através do buildAndExpand e converte ele para URI */
+		return ResponseEntity.created(uri).build();
+		/* retorna o created baseado na URI montada*/
+	}
+	
+	
 	
 }
--- src/main/java/com/vizoni/cursomc/services/PedidoService.java
+++ src/main/java/com/vizoni/cursomc/services/PedidoService.java
@@ -1,10 +1,18 @@
 package com.vizoni.cursomc.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vizoni.cursomc.domain.ItemPedido;
+import com.vizoni.cursomc.domain.PagamentoComBoleto;
 import com.vizoni.cursomc.domain.Pedido;
+import com.vizoni.cursomc.domain.enums.EstadoPagamento;
+import com.vizoni.cursomc.repositories.ItemPedidoRepository;
+import com.vizoni.cursomc.repositories.PagamentoRepository;
 import com.vizoni.cursomc.repositories.PedidoRepository;
+import com.vizoni.cursomc.repositories.ProdutoRepository;
 import com.vizoni.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -12,14 +20,45 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
-	
+
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id +
-											  ", Tipo: " + Pedido.class.getName());
+			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Pedido.class.getName());
 		}
 		return obj;
 	}
-	
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE); // pedido q acabou de inserir o pagamento é pendente
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.save(obj.getItens());
+		return obj;
+	}
+
 }
